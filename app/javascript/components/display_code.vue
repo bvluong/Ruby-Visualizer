b6c23e0ef773ea75c91ec746fb6cea76c0052fbe
Audrey Mefford <acmefford@gmail.com>
2017-06-05 11:50:27 -0700

    Add svg to display

--- app/javascript/components/display_code.vue
+++ app/javascript/components/display_code.vue
@@ -1,12 +1,30 @@
 <template>
   <div id='display'>
-    <h1>display area</h1>
-    
+    <p>
+      {{stack}}
+    </p>
+    <svg width="500" height="500">
+    <g><text x='0' y='20'>Frame information</text></g>
+      <circle cx="50" cy="50" r="5" stroke="green" stroke-width="4" fill="yellow" />
+      <canvas></canvas>
+    <g><text x='250' y='20'>Detail</text></text></g>
+    </svg>
   </div>
 </template>
 
+<script>
+  export default {
+    computed: {
+      stack () {
+        return this.$store.state.code[]
+      }
+    },
+  }
+</script>
 
-export default
-<style>
 
+<style scoped>
+  #display{
+    background-color: pink;
+  }
 </style>
