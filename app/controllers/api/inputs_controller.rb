a684962fbd9ed38b27164f4d39bb56f34519c6f9
bvluong <bryantvinluong@gmail.com>
2017-06-05 20:19:31 -0700

    Fix blocks for evaluator

--- app/controllers/api/inputs_controller.rb
+++ app/controllers/api/inputs_controller.rb
@@ -35,36 +35,70 @@ class Eval
 
   def trace
     counter = 0
+    block_lines = getBlockLineNumbers
     tracer = TracePoint.new(:line) do |tp|
       counter += 1
-      [tp.event]
-      retrieve_variables(tp.lineno)
+      if block_lines.any? { |x,y| tp.lineno.between?(x,y)}
+        retrieve_variables(tp.lineno, true)
+      else
+        retrieve_variables(tp.lineno)
+      end
     end.enable do
       evaluate
     end
   end
 
-  def retrieve_variables(lineno)
+  def getBlockLineNumbers
+    startnumbers = []
+    endnumbers = []
+    trace2 = TracePoint.trace(:b_call) do |tp2|
+      unless startnumbers.include?(tp2.lineno)
+        startnumbers << tp2.lineno
+      end
+    end.enable do
+      evaluate
+    end
+    trace3 = TracePoint.trace(:b_return) do |tp3|
+      unless endnumbers.include?(tp3.lineno)
+        endnumbers << tp3.lineno
+      end
+    end.enable do
+      evaluate
+    end
+    startnumbers.pop
+    startnumbers.shift
+    endnumbers.pop
+    return [startnumbers,endnumbers].transpose
+  end
+
+
+  def retrieve_variables(lineno, blocks = false)
     count = 2
     unless ["trace", "evaluate"].include?(grabMethodName(count))
       object = {}
       stack_frame = MyStack.new
-
+      swap = false
       until grabMethodName(count) == "evaluate"
-        unless binding.of_caller(count-1).eval('local_variables').include?(:tracer)
-          blockObj = {}
-          blockObj['method_name'] = 'block'
-          binding.of_caller(count-1).eval('local_variables').each do |var2|
-            blockObj[var2] = binding.of_caller(count).eval(var2.to_s)
+        # if blocks
+        #   blockObj = {}
+        #   swap ? blockObj['method_name'] = grabMethodName(count) : blockObj['method_name'] = 'block'
+        #   swap = true
+        #   binding.of_caller(count).eval('local_variables').each do |var2|
+        #     blockObj[var2] = binding.of_caller(count+1).eval(var2.to_s)
+        #   end
+        #   stack_frame.push(blockObj)
+        # else
+          functionObj = {}
+          functionObj['method_name'] = grabMethodName(count)
+          binding.of_caller(count).eval('local_variables').each do |var|
+            if binding.of_caller(count+1).eval(var.to_s).is_a?(Array)
+              functionObj[var] = binding.of_caller(count+1).eval(var.to_s).dup
+            else
+              functionObj[var] = binding.of_caller(count+1).eval(var.to_s)
+            end
           end
-          stack_frame.push(blockObj)
-        end
-
-        functionObj = {}
-        binding.of_caller(count).eval('local_variables').each do |var|
-          functionObj[var] = binding.of_caller(count+1).eval(var.to_s)
-        end
-        stack_frame.push(functionObj)
+          stack_frame.push(functionObj)
+        # end
         count += 1
       end
 
