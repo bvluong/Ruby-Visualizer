bfa7b600a129fc154d237ad98560a1ae0daa766a
bvluong <bryantvinluong@gmail.com>
2017-06-06 20:00:29 -0700

    Add stack frame display

--- app/javascript/components/code_input.vue
+++ app/javascript/components/code_input.vue
@@ -7,12 +7,16 @@
         lang="ruby" theme="github"  height="400" width="500px"></Editor>
       </div>
 
+      <div>
+        Step {{stackFrame}} of {{ codeLength }}
+      </div>
+
       <section class="input-buttons">
         <button type="button" name="button"
         @click="moveBackward">Backward
         </button>
         <button type="button" name="button"
-        @click="submitCode(userInput)">Run Code
+        @click="runCode(userInput)">Run Code
         </button>
         <button type="button" name="button"
         @click="moveForward">Forward
@@ -39,7 +43,14 @@ export default {
      return this.$store.state.code
    },
     currentFrame () {
-      return this.forwardStack[0] ? this.forwardStack[0] : {}
+      return this.forwardStack[0] ? this.forwardStack[0] : false
+    },
+    codeLength () {
+      if (this.forwardStack[0]) {
+        return (this.forwardStack.length + this.backwardStack.length);
+      } else {
+        return 0;
+      }
     }
   },
   data: function () {
@@ -55,7 +66,8 @@ export default {
               end
               test([1,1,3,4])`,
       backwardStack: [],
-      previousLine: false
+      previousLine: false,
+      stackFrame: 0
     }
   },
   components: { Editor, DisplayCode },
@@ -66,20 +78,28 @@ export default {
     moveForward: function () {
       if (this.forwardStack.length > 1) {
         this.backwardStack.push(this.forwardStack.shift())
+        this.stackFrame +=1
       }
     },
     moveBackward: function () {
       if (this.backwardStack.length > 0) {
         this.forwardStack.unshift(this.backwardStack.pop())
+        this.stackFrame -= 1
       }
     },
     runCode: function(userInput) {
+      this.stackFrame = 1
+      this.forwardStack = []
+      this.backwardStack = []
       this.submitCode(userInput)
     },
     selectLine() {
       var editor = ace.edit('editor')
-      const lineno = parseInt(Object.keys(this.currentFrame)[0].slice(6))
-      editor.selection.moveCursorToPosition({row: lineno-1, column: 0})
+      if (this.currentFrame) {
+        const lineno = parseInt(Object.keys(this.currentFrame)[0].slice(6))
+        editor.selection.clearSelection();
+        editor.selection.moveCursorToPosition({row: lineno-1, column: 0})
+      }
     },
     ...mapActions(['submitCode'])
   }
