afaaa6cdd16c393f34c99bffdd2044988c0a9e7a
bvluong <bryantvinluong@gmail.com>
2017-06-06 20:49:50 -0700

    Fix stack frame not allowing edit

--- app/javascript/components/code_input.vue
+++ app/javascript/components/code_input.vue
@@ -1,27 +1,25 @@
 <template lang="html">
 
-  <div id='body-section'>
+  <div class='body-section'>
     <div class='code-input'>
-      <div ref='editor'>
         <Editor  id='editor' v-model="userInput"
-        lang="ruby" theme="github"  height="400" width="500px"></Editor>
-      </div>
-
-      <div>
-        Step {{stackFrame}} of {{ codeLength }}
-      </div>
+        lang="ruby" theme="github"  height="400" width="100%"></Editor>
 
       <section class="input-buttons">
         <button type="button" name="button"
         @click="moveBackward">Backward
         </button>
-        <button type="button" name="button"
-        @click="runCode(userInput)">Run Code
-        </button>
+        <div class="frame-count">
+          Step {{stackFrame}} of {{ codeLength }}
+        </div>
         <button type="button" name="button"
         @click="moveForward">Forward
         </button>
       </section>
+
+      <button type="button" name="button"
+      @click="runCode(userInput)">Run Code
+      </button>
 
     </div>
     <div id='display-code'>
@@ -60,31 +58,38 @@ export default {
                 h = {two: 2}
                 x = 'test'
                 y = 1.3
-                arr.map {|x| x*2}
+                (0..5).step do |idx|
+                  y += idx
+                end
                 z = 2
                 return x+y
               end
               test([1,1,3,4])`,
       backwardStack: [],
       previousLine: false,
-      stackFrame: 0
+      stackFrame: 0,
+      buttonUpdate: true
     }
   },
   components: { Editor, DisplayCode },
   updated: function () {
-    this.selectLine()
+    if (this.buttonUpdate) {
+      this.selectLine()
+    }
   },
   methods: {
     moveForward: function () {
       if (this.forwardStack.length > 1) {
         this.backwardStack.push(this.forwardStack.shift())
         this.stackFrame +=1
+        this.buttonUpdate = true
       }
     },
     moveBackward: function () {
       if (this.backwardStack.length > 0) {
         this.forwardStack.unshift(this.backwardStack.pop())
         this.stackFrame -= 1
+        this.buttonUpdate = true
       }
     },
     runCode: function(userInput) {
@@ -92,6 +97,7 @@ export default {
       this.forwardStack = []
       this.backwardStack = []
       this.submitCode(userInput)
+      this.buttonUpdate = true
     },
     selectLine() {
       var editor = ace.edit('editor')
@@ -99,6 +105,9 @@ export default {
         const lineno = parseInt(Object.keys(this.currentFrame)[0].slice(6))
         editor.selection.clearSelection();
         editor.selection.moveCursorToPosition({row: lineno-1, column: 0})
+        if (this.stackFrame != 1) {
+          this.buttonUpdate = false
+        }
       }
     },
     ...mapActions(['submitCode'])
