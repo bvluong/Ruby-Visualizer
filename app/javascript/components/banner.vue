4b5dbb7f6e8d4a3622499d7a3588b9829002f38f
bvluong <bryantvinluong@gmail.com>
2017-06-10 18:02:17 -0700

    Remove unused code

--- app/javascript/components/banner.vue
+++ app/javascript/components/banner.vue
@@ -83,16 +83,3 @@ check_anagram("brite", "tribe")`
   }
 }
 </script>
-
-<style scoped>
-  /*Style does not work*/
-  header {
-    height: 200px;
-    width: 100%;
-    border: 1px solid blue;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-  }
-
-</style>
