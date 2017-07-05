5c142f85b6fa4c7f47b81f5fd4a1c193b9168758
Audrey Mefford <acmefford@gmail.com>
2017-07-05 13:55:20 -0700

    Add instructions on page load

--- app/javascript/components/display_code.vue
+++ app/javascript/components/display_code.vue
@@ -2,6 +2,26 @@
   <div id='display'>
     <h2 class='green'>{{errors}}</h2>
     <h2 class='green'>{{returnValue}}</h2>
+    <div class='instructions' v-if='!frame'>
+      <h3>Welcome to Ruby Visualizer.</h3>
+
+      <h4>This site is for coders learning the Ruby programming language.</h4>
+
+      Instructions:
+      <ul>
+        <li>
+          Input your code in the text editor to the left (or click on one of our example algorithms on the top right of this page).
+        </li>
+        <li>
+          Once your code is ready to run, click the 'Compile' button.
+        </li>
+        <li>
+          You will start to see information about your code being displayed on the right side of the page. Use the arrow buttons to navigate through each line of your code and see how your variables change.
+        </li>
+      </ul>
+
+
+    </div>
     <ul class='function-list' v-for="(stack, idx) in stackFrame">
         <Frame :stacks='stack' :numWidth= 'idx' :stackNum='stackFrame.length - idx' :isCurrentFrame='idx === 0'></Frame>
     </ul>
@@ -37,9 +57,3 @@ import Frame from './frame'
     props: ['frame']
   }
 </script>
-
-<style module>
-  .red{
-    color: red;
-  }
-</style>
